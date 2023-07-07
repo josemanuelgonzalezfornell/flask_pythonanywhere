87a9707d64c6d6e9885166c138d4dc54775b6bc0
José Manuel González Fornell <63046689+josemanuelgonzalezfornell@users.noreply.github.com>
2023-07-07 19:50:09 +0200

    Se ha subido el archivo app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home_page():
+    message="Welcome to the Home Page"
+    response={"message":message}
+    return jsonify({"response":response}), 200
+
+# Cargamos el modelo
+# with open('model.pkl', 'rb') as f:
+#     model = pickle.load(f)
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.json
+#     prediction = model.predict(np.array(data['example']).reshape(1, -1))
+#     return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run()
+
